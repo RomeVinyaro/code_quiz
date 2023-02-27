eb6dc1efaba40d506494b415cafe59487bde9d37
Jerome <jerome.p.egbujor@gmail.com>
2023-02-27 12:37:19 -0600

    Created the questions for javascript

---
+++ assets/js/questions.js
@@ -0,0 +1,46 @@
+var questions = [
+    {
+        question: "What does HTML stand for?",
+        answers: [
+            { Text: "Home Tool Markup Language", correct: false},
+            { Text: "Hyper Text Markup Language", correct: true},
+            { Text: "Hyperlinks and Text Markup Language", correct: false},
+            { Text: "Hello To My Love", correct: false},
+        ]
+      
+    },
+    {
+        question: "Inside which HTML element do we put the Javascript?",
+        answers: [
+            {Text: "<script>", correct: true},
+            {Text: "<javascript>", correct: false},
+            {Text: "<scripting>", correct: false},
+            {Text: "<js>" correct: false},
+        ]
+    },
+    {
+        question: "HTML comments start with ./",
+        answers: [
+            {Text: "True", correct: false},
+            {Text: "False", correct: true},
+        ]
+    },
+    {
+        question: "How do you create a function in Javascript?",
+        answers: [
+            {Text: "function = myFunction()", correct: true},
+            {Text: "function:myFunction()", correct: false},
+            {Text: "function myFunction()", correct: false},
+            {Text: "function = ()", correct: false},
+        ]
+    },
+    {
+        question: "How can you make a numbered list?",
+        answers: [
+            {Text: "<list>", correct: false},
+            {Text: "<ul>", correct: false},
+            {Text: "<ol>", correct: true},
+            {Text: "<nl>", correct: false},
+        ]
+    }
+];
